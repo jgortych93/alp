28c831f05c85537e0395f874d84089ffd0b674bc
kuba <kurt456@gmail.com>
2015-12-22 16:40:34 +0100

    net_mod bez mknod

--- src/net_mod.c
+++ src/net_mod.c
@@ -14,7 +14,7 @@
 MODULE_LICENSE("GPL");
 static int Device_Open = 0;
 static char* Message_Ptr;
-
+/*
 static int device_open(struct inode *inode, struct file *file)
 {
 #ifdef DEBUG
@@ -31,7 +31,7 @@ static int device_open(struct inode *inode, struct file *file)
 	try_module_get(THIS_MODULE);
 	return SUCCESS;
 }
-
+*/
 
 static ssize_t device_read (struct file *file,	char __user * buffer,	size_t length,int option, loff_t * offset)
 {
@@ -61,36 +61,31 @@ struct file_operations Fops = {
 	.read = device_read,
 	.write = NULL,
 	.unlocked_ioctl = NULL,
-	.open = device_open,
+	.open = NULL,
 	.release = NULL,	/* a.k.a. close */
 };
 
 int init_module()
 {
-	
+	int ret_val;
 	dev_t* first;
 	struct class* cl;
 	struct cdev *  	c_dev;
 	/* 
 	 * Register the character device (atleast try) 
 	 */
-	//ret_val = register_chrdev(0, DEVICE_NAME, &Fops);
+	ret_val = register_chrdev(0, DEVICE_NAME, &Fops);
 
-	/* 
-	 * Negative values signify an error 
+	 
+	// * Negative values signify an error 
 	 
 	if (ret_val < 0) {
 		printk(KERN_ALERT "%s failed with %d\n",
 		       "Sorry, registering the character device ", ret_val);
 		return ret_val;
 	}
-	dev=makedev(666, 1);
-	mkn=mknod("/dev/char_dev", S_IFCHR ,dev);
-	if (mkn < 0) {
-        perror("mknod");
-        return -1;
-    }
-	*/
+	
+	/*
 	 printk(KERN_INFO "Welcome!");
     if (alloc_chrdev_region(&first, 0, 1, "char_dev") < 0)  //$cat /proc/devices
     {
@@ -101,12 +96,7 @@ int init_module()
         unregister_chrdev_region(first, 1);
         return -1;
     }
-    if (device_create(cl, NULL, first, NULL, "mynull") == NULL) //$ls /dev/
-    {
-        class_destroy(cl);
-        unregister_chrdev_region(first, 1);
-        return -1;
-    }
+    
     cdev_init(&c_dev, &Fops);
     if (cdev_add(&c_dev, first, 1) == -1)
     {
@@ -116,5 +106,10 @@ int init_module()
         return -1;
     }
 
-
+	if (device_create(cl, NULL, first, NULL, "mynull") == NULL) //$ls /dev/
+    {
+        class_destroy(cl);
+        unregister_chrdev_region(first, 1);
+        return -1;
+    }*/
 }
